00f052328ab9ef8e5e11089d755869907f8cec4e
Mike Loseto <losetom1998@gmail.com>
2022-11-03 20:04:40 -0400

    Completed Assignment 2.0

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
+    # retrieving all items 
     def index
+        # calling all items
         items = Item.all
+        #render as JSON, include users via serialization
         render json: items, include: :user
     end
 end
